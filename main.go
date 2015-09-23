c6ea265a8c0040438e0ef64573625ecf672b2d7f
Dusan Lilic <kortemy@gmail.com>
2015-09-23 15:54:09 +0200

    That is all

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := flag.String("port", "9090", "Default serve port")
+	wd, _ := os.Getwd()
+
+	flag.Parse()
+
+	fs := http.FileServer(http.Dir(wd))
+	http.Handle("/", fs)
+
+	log.Println("Serving in " + wd + " on " + *port)
+	http.ListenAndServe(":"+*port, nil)
+}
